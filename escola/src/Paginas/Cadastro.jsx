f0fb355ec61c19190eb7744c216ca04f99512372
larissaliper <larissaliper3@gmail.com>
2024-11-18 09:46:05 -0300

    gui arrumou

---
+++ escola/src/Paginas/Cadastro.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+export default function Cadastro()
+{    
+    return (
+        <div>            
+            
+        </div>
+    );
+}
---
+++ escola/src/Paginas/Consulta.jsx
@@ -0,0 +1,9 @@
+export default function Consulta()
+{
+    
+    return (
+        <div>
+            <h1>Consulta de Alunos</h1>
+        </div>
+    );
+}
